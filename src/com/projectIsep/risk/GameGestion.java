a1226cb960fa5c8a2ed10aa02ffeeac6a1eb5e3e
Raquel De Faria <racheldf19@gmail.com>
2018-05-26 16:36:32 +0200

    Territories

--- src/com/projectIsep/risk/GameGestion.java
+++ src/com/projectIsep/risk/GameGestion.java
@@ -30,40 +30,17 @@ public class GameGestion {
         }
 
         // ---------- Initialising the background---------- //
-        initiateBackground(territoryArrayList);
+        initiateBackground(territoryArrayList,numberOfPlayers);
+
+        // ---------- Initialising the background---------- //
+        StdDraw.pause(1000);
+        StdDraw.clear();
+        StdDraw.text(50,50,"Please put your armies in the territories");
+        StdDraw.pause(2000);
 
         // ---------- Initialising Armies in the different territories ---------- //
-        for (int k = 0; k<playerArrayList.size();k++){
-            Player player = playerArrayList.get(k);
-            StdDraw.pause(2000);
-            StdDraw.clear();
-            StdDraw.text(50,50,"Please put your armies in the territories");
-            StdDraw.pause(2000);
-            updateBackground(territoryArrayList);
-            player.setReinforcement(player.getReinforcement()-player.getArraylistTerritories().size());
-            System.out.println("reinforcements = "+player.getReinforcement());
-            while (player.getReinforcement()!=0) {
-                // going through the player's territories when there is a click
-                if (StdDraw.isMousePressed()) {
-                    //coordinates of the click
-                    double x = StdDraw.mouseX();
-                    double y = StdDraw.mouseY();
-                    StdDraw.pause(200);
-                    for (int u = 0; u < player.getArraylistTerritories().size(); u++) {
-                        Territory territory = player.getArraylistTerritories().get(u);
-                        if ((x >= territory.getX() - 2 && x <= territory.getX() + 2) && (y >= territory.getY() - 4 && y <= territory.getY() + 4)) {
-                            territory.setNbSoldier(territory.getNbSoldier() + 1);
-                            player.setReinforcement(player.getReinforcement() - 1);
-                            System.out.println(player.getReinforcement());
-                            updateBackground(territoryArrayList);
-                        }
+        initiateArmy(playerArrayList, territoryArrayList);
 
-                    }
-
-                }
-            }
-            updateBackground(territoryArrayList);
-        }
 
         while (!gameOver) {
             StdDraw.pause(400);
@@ -104,16 +81,170 @@ public class GameGestion {
         }
     }
 
+    // ---------- Initialising Armies in the different territories ---------- //
+    public void initiateArmy(ArrayList<Player> playerArrayList, ArrayList<Territory> territoryArrayList) {
+
+        updateBackground(territoryArrayList, playerArrayList.size());
+
+        for (int k = 0; k<playerArrayList.size();k++){
+            Player player = playerArrayList.get(k);
+
+            //updating player's reinforcements with 1 unit in each territory
+            player.setReinforcement(player.getReinforcement()-player.getArraylistTerritories().size());
+
+            while (player.getReinforcement()!=0) {
+                // going through the player's territories when there is a click
+                if (StdDraw.isMousePressed()) {
+                    //coordinates of the click
+                    double x = StdDraw.mouseX();
+                    double y = StdDraw.mouseY();
+                    StdDraw.pause(200);
+                    System.out.println(x);
+                    System.out.println(y);
+                    // clicked on the menu panel
+                    if(x>=89) {
+                        // clicking on the mission
+                        if (y >= 43.96 && y <= 46.96) {
+                            StdDraw.clear();
+                            StdDraw.text(50, 50, "Player " + (k + 1) + ", your mission is :  " + player.getMission().getBriefing());
+                            StdDraw.pause(5000);
+                            StdDraw.clear();
+                            updateBackground(territoryArrayList, playerArrayList.size());
+                        }
+                        //clicking on inspection
+                        else {
+                            StdDraw.clear();
+                            StdDraw.text(50,50,"Click on a territory to inspect it !");
+                            StdDraw.pause(2000);
+                            updateBackground(territoryArrayList, playerArrayList.size());
+                            boolean territoryChecked = false;
+                            while (!territoryChecked) {
+                                if (StdDraw.isMousePressed()) {
+                                    //coordinates of the click
+                                    double xTerritory = StdDraw.mouseX();
+                                    double yTerritory = StdDraw.mouseY();
+                                    StdDraw.pause(200);
+                                    for (int u = 0; u < territoryArrayList.size(); u++) {
+                                        Territory territory = territoryArrayList.get(u);
+                                        if ((xTerritory >= territory.getX() - 2 && xTerritory <= territory.getX() + 2) && (yTerritory >= territory.getY() - 4 && yTerritory <= territory.getY() + 4)) {
+                                            StdDraw.clear();
+                                            StdDraw.text(50, 60, "Here are the units available in " + territory.getNameTerritory());
+                                            StdDraw.text(30, 50, "Number of Soldiers : " + territory.getNbSoldier());
+                                            StdDraw.text(50, 50, "Cavalry effective : " + territory.getNbCavalery());
+                                            StdDraw.text(70, 50, "Number of Canons : " + territory.getNbCanon());
+                                            StdDraw.pause(5000);
+                                            territoryChecked = true;
+                                            updateBackground(territoryArrayList, playerArrayList.size());
+                                        }
+                                    }
 
 
+                                }
+                            }
+                        }
+                    }
+                    //clicked on the map
+                    else {
+                        for (int u = 0; u < player.getArraylistTerritories().size(); u++) {
+                            Territory territory = player.getArraylistTerritories().get(u);
+                            if ((x >= territory.getX() - 2 && x <= territory.getX() + 2) && (y >= territory.getY() - 4 && y <= territory.getY() + 4)) {
+                                boolean unitChosen = false;
+                                StdDraw.clear();
+                                StdDraw.setPenColor(Color.BLACK);
+                                StdDraw.text(50, 90, "Please choose what type of unit you want to add to " + territory.getNameTerritory());
+                                StdDraw.text(50, 85, "You have " + player.getReinforcement() + " points of reinforcement left");
+                                StdDraw.text(20, 35, "Points of reinforcement : 3");
+                                StdDraw.text(50, 35, "Points of reinforcement : 1");
+                                StdDraw.text(80, 35, "Points of reinforcement : 7");
+                                StdDraw.picture(20, 50, "img/cavalery.png");
+                                StdDraw.picture(50, 50, "img/soldier.png");
+                                StdDraw.picture(80, 50, "img/canon.png");
+                                while (!unitChosen) {
+                                    if (StdDraw.isMousePressed()) {
+                                        //coordinates of the click
+                                        double xUnit = StdDraw.mouseX();
+                                        double yUnit = StdDraw.mouseY();
+                                        StdDraw.pause(200);
+                                        if (yUnit >= 40 && yUnit <= 60) {
+                                            if ((xUnit >= 13 && xUnit <= 26) & player.getReinforcement() >= 3) {
+                                                territory.setNbCavalery(territory.getNbCavalery() + 1);
+                                                player.setReinforcement(player.getReinforcement() - 3);
+                                                unitChosen = true;
+                                            } else if ((xUnit >= 44 && xUnit <= 57) && player.getReinforcement() >= 1) {
+                                                territory.setNbSoldier(territory.getNbSoldier() + 1);
+                                                player.setReinforcement(player.getReinforcement() - 1);
+                                                unitChosen = true;
+                                            } else if ((xUnit >= 73 && xUnit <= 87) && player.getReinforcement() >= 7) {
+                                                territory.setNbCanon(territory.getNbCanon() + 1);
+                                                player.setReinforcement(player.getReinforcement() - 7);
+                                                unitChosen = true;
+                                            }
+                                        }
+                                    }
+                                }
+                                updateBackground(territoryArrayList, playerArrayList.size());
+                            }
 
-    public void initiateBackground(ArrayList<Territory> territoryArrayList){
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void initiateBackground(ArrayList<Territory> territoryArrayList, int numberOfPlayers ){
         // map of the world
         StdDraw.setCanvasSize(1200,737);
         StdDraw.setXscale(0,100);
         StdDraw.setYscale(0,100);
         StdDraw.clear();
         StdDraw.picture(50,50, "img/riskmap.jpg");
+        for(int a=0;a<numberOfPlayers;a++){
+            if(a==0) {
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.filledSquare(91,85,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,85, " - Player " + (a+1));
+            }
+            else if(a==1) {
+                StdDraw.setPenColor(Color.GREEN);
+                StdDraw.filledSquare(91,81,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,81, " - Player " + (a+1));
+            }
+            else if(a==2) {
+                StdDraw.setPenColor(Color.BLUE);
+                StdDraw.filledSquare(91,77,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,77, " - Player " + (a+1));
+            }
+            else if(a==3) {
+                StdDraw.setPenColor(Color.MAGENTA);
+                StdDraw.filledSquare(91,73,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,73, " - Player " + (a+1));
+            }
+            else if(a==4) {
+                StdDraw.setPenColor(Color.ORANGE);
+                StdDraw.filledSquare(91,69,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,69, " - Player " + (a+1));
+            }
+            else if(a==5) {
+                StdDraw.setPenColor(Color.GRAY);
+                StdDraw.filledSquare(91,65,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,65, " - Player " + (a+1));
+            }
+
+        }
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.text(94,60,"Attack");
+        StdDraw.text(94,56,"Move");
+        StdDraw.text(94,52,"Pass");
+        StdDraw.text(94,45,"Mission");
+        StdDraw.text(94,41,"Inspection");
         double [] xList = {2.75, 14.666666666666666, 12.666666666666666, 17.166666666666668, 23.666666666666668, 29.333333333333332, 12.333333333333334, 22.0, 11.333333333333334,
                 18.333333333333332, 27.0, 18.916666666666668, 20.833333333333332, 48.25, 47.416666666666664, 58.666666666666664, 52.583333333333336, 44.5, 39.916666666666664,
                 37.083333333333336, 38.25, 41.083333333333336, 37.333333333333336, 45.666666666666664, 43.666666666666664, 51.333333333333336, 53.833333333333336, 59.333333333333336,
@@ -154,13 +285,7 @@ public class GameGestion {
                 StdDraw.setPenColor(Color.ORANGE);
             }
             else if(territoryArrayList.get(it).getProprietary()==6){
-                StdDraw.setPenColor(Color.PINK);
-            }
-            else if(territoryArrayList.get(it).getProprietary()==7){
                 StdDraw.setPenColor(Color.GRAY);
-            }
-            else if(territoryArrayList.get(it).getProprietary()==8){
-                StdDraw.setPenColor(Color.YELLOW);
             }
             else{
                 StdDraw.setPenColor(Color.BLACK);
@@ -173,9 +298,9 @@ public class GameGestion {
             int numberCanons = territoryArrayList.get(it).getNbCanon();
             int numberCavalery = territoryArrayList.get(it).getNbCavalery();
             int numberSoldier = territoryArrayList.get(it).getNbSoldier();
-            int numbreArmies = numberCanons + numberCavalery + numberSoldier;
+            int numberArmies = 7*numberCanons + 3*numberCavalery + numberSoldier;
             // showing the number of armies in each territory
-            StdDraw.text(x,y, String.valueOf(numbreArmies));
+            StdDraw.text(x,y, String.valueOf(numberArmies));
         }
 
         // display and pause for 20 ms
@@ -185,9 +310,53 @@ public class GameGestion {
 
     }
 
-    public void updateBackground(ArrayList<Territory> territoryArrayList){
+    public void updateBackground(ArrayList<Territory> territoryArrayList, int numberOfPlayers){
         StdDraw.clear();
         StdDraw.picture(50,50, "img/riskmap.jpg");
+        for(int a=0;a<numberOfPlayers;a++){
+            if(a==0) {
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.filledSquare(91,85,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,85, " - Player " + (a+1));
+            }
+            else if(a==1) {
+                StdDraw.setPenColor(Color.GREEN);
+                StdDraw.filledSquare(91,81,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,81, " - Player " + (a+1));
+            }
+            else if(a==2) {
+                StdDraw.setPenColor(Color.BLUE);
+                StdDraw.filledSquare(91,77,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,77, " - Player " + (a+1));
+            }
+            else if(a==3) {
+                StdDraw.setPenColor(Color.MAGENTA);
+                StdDraw.filledSquare(91,73,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,73, " - Player " + (a+1));
+            }
+            else if(a==4) {
+                StdDraw.setPenColor(Color.ORANGE);
+                StdDraw.filledSquare(91,69,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,69, " - Player " + (a+1));
+            }
+            else if(a==5) {
+                StdDraw.setPenColor(Color.GRAY);
+                StdDraw.filledSquare(91,65,1);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.text(95,65, " - Player " + (a+1));
+            }
+
+        }
+        StdDraw.text(94,60,"Attack");
+        StdDraw.text(94,56,"Move");
+        StdDraw.text(94,52,"Pass");
+        StdDraw.text(94,45,"Mission");
+        StdDraw.text(94,41,"Inspection");
         double [] xList = {2.75, 14.666666666666666, 12.666666666666666, 17.166666666666668, 23.666666666666668, 29.333333333333332, 12.333333333333334, 22.0, 11.333333333333334,
                 18.333333333333332, 27.0, 18.916666666666668, 20.833333333333332, 48.25, 47.416666666666664, 58.666666666666664, 52.583333333333336, 44.5, 39.916666666666664,
                 37.083333333333336, 38.25, 41.083333333333336, 37.333333333333336, 45.666666666666664, 43.666666666666664, 51.333333333333336, 53.833333333333336, 59.333333333333336,
@@ -242,9 +411,9 @@ public class GameGestion {
             int numberCanons = territoryArrayList.get(it).getNbCanon();
             int numberCavalery = territoryArrayList.get(it).getNbCavalery();
             int numberSoldier = territoryArrayList.get(it).getNbSoldier();
-            int numbreArmies = numberCanons + numberCavalery + numberSoldier;
+            int numberArmies = 7*numberCanons + 3*numberCavalery + numberSoldier;
             // showing the number of armies in each territory
-            StdDraw.text(x,y, String.valueOf(numbreArmies));
+            StdDraw.text(x,y, String.valueOf(numberArmies));
         }
 
         // display and pause for 20 ms
@@ -252,7 +421,6 @@ public class GameGestion {
         //StdDraw.pause(20);
 
     }
-
 
     public ArrayList<Territory> initiateTerritories(){
         ArrayList<Territory> territoryArrayList = new ArrayList<>();
@@ -328,7 +496,6 @@ public class GameGestion {
                 player.setReinforcement(40);
                 for (int twoPlayers = 0; twoPlayers < numberTerritories/2; twoPlayers++){
                     int territoryId2 = random.nextInt(numberTerritories);
-                    System.out.println(territoryId2);
                     addTerritory(player, playerId,territoryId2,territories);
                 }
                 break;
@@ -384,7 +551,7 @@ public class GameGestion {
 
     public void conflict(Territory attackerTerritory, Territory defenderTerritory){
 
-                    // ------------------- On crée l'armée attaquante -------------------//
+        // ------------------- On crée l'armée attaquante -------------------//
         Army attacker = new Army();
         attacker.setAttacker(true);
         attacker.setTerritory(attackerTerritory);
@@ -392,7 +559,7 @@ public class GameGestion {
             conflict(attackerTerritory,defenderTerritory); // on reboot
         }
 
-                    // ------------------- On crée l'armée défenseuse -------------------//
+        // ------------------- On crée l'armée défenseuse -------------------//
         Army defender = new Army();
         defender.setAttacker(false);
         defender.setTerritory(defenderTerritory);
@@ -400,7 +567,7 @@ public class GameGestion {
             conflict(attackerTerritory,defenderTerritory); // on reboot
         }
 
-                    // ------------------- On résout un round de combat -------------------//
+        // ------------------- On résout un round de combat -------------------//
 
         attacker.battle(defender);
 
